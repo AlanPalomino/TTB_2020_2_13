a04178f9b5d19a2f3c1947ddb85da79088615bca
AlanPalomino <zappataville@gmail.com>
2020-11-04 21:57:38 -0600

    Initial Commit

---
+++ code_tests.py
@@ -0,0 +1,11 @@
+# %%
+#_
+#___________________________________________________________________________
+#|                                                                         |
+#| Playground para pruebas:                                                |
+#|      De ser necesario probar código, este script puede ser usaado       |
+#|      para ese propósito. El archivo se eliminará del repositorio        |
+#|      una vez concluido el proyecto.                                     |
+#|_________________________________________________________________________|
+
+# %%
