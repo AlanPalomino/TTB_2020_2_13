e752f017de34336864733f3ef704e3c2876be2fd
AlanPalomino <zappataville@gmail.com>
2020-11-21 13:20:57 -0600

    update

--- main.py
+++ main.py
@@ -9,3 +9,15 @@
 #|_________________________________________________________________________|
 
 # %%
+# ===================== Librerias Utilizadas ====================== #
+from matplotlib import pyplot as plt
+from scipy.stats import stats
+import pandas as pd
+import numpy as np
+import decimal
+import ast
+#import wfdb
+from pprint import pprint 
+import entropy as tpy
+
+# ================================================================= #
