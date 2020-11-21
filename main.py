c6d78d39f7a1934635742653e4953398cb516790
AlanPalomino <zappataville@gmail.com>
2020-11-21 13:28:14 -0600

    update

--- main.py
+++ main.py
@@ -10,14 +10,24 @@
 
 # %%
 # ===================== Librerias Utilizadas ====================== #
+
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
+from matplotlib import gridspec
 from scipy.stats import stats
 from pprint import pprint 
 import entropy as tpy
 import pandas as pd
 import numpy as np
 import decimal
+import json
+import wfdb
 import ast
-#import wfdb
+import os
+
+
+
+
+
 
 # ================================================================= #
