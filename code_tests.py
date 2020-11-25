01bec1edb77d3c7161129d152ea251a4126d1e55
AlanPalomino <zappataville@gmail.com>
2020-11-25 11:33:29 -0600

    update

--- code_tests.py
+++ code_tests.py
@@ -9,9 +9,26 @@
 #|_________________________________________________________________________|
 
 import re
-from TT import Case, Case2
+#from TT import Case, Case2
 from pathlib import Path
-from memory_profiler import profile
+
+from wfdb.processing.qrs import gqrs_detect
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot as plt
+#from memory_profiler import profile
+from matplotlib import gridspec
+from scipy.stats import stats
+
+from pprint import pprint 
+import entropy as tpy
+import pandas as pd
+import numpy as np
+import decimal
+import json
+import wfdb
+import ast
+import os
+import re
 
 
 # %%
@@ -35,6 +52,39 @@ import pyhrv.tools as tools
 import pyhrv.time_domain as td
 import pyhrv.nonlinear as nl
 
+# %%
+
+data = list()
+for data_file in os.listdir("./Data_Jsons"):
+    with open("./Data_Jsons/"+data_file) as file:
+        mixed = json.load(file)
+        appr = [reg for reg in mixed if reg["approved"]]
+        data.extend(appr)
+        print(f"{data_file} has {len(appr)}/{len(mixed)} approved cases")
+data = pd.DataFrame(data)
+data["rr"] = data.apply(lambda case: np.array(case["rr"])/case["fs"], axis=1)
+data["rr"] = data["rr"].apply(lambda signal: signal[np.where(signal < 2)])
+data["length"] = data["rr"].apply(lambda signal: len(signal))
+
+print("Seleccion de casos aprobados...")
+num_cases = 15
+# AF - Atrial Fibrilation
+AF_CASES = data[(data["conditon"] == "AF") & (data["length"] > 1000)][:num_cases]
+# CHF - Congestive Heart Failure
+CHF_CASES = data[(data["conditon"] == "CHF") & (data["length"] > 1000)][:num_cases]
+# HC - Healthy Controls
+HC_CASES = data[(data["conditon"] == "HC") & (data["length"] > 1000)][:num_cases]
+# AR - Arrhythmia Cases
+AR_CASES = data[(data["conditon"] == "AR") & (data["length"] > 1000)][:num_cases]   # NO HAY CASOS QUE CUMPLAN 
+# MI - Myocardial Infarction
+MI_CASES = data[(data["conditon"] == "MI") & (data["length"] > 1000)][:num_cases]   # NO HAY CASOS QUE CUMPLAN
+print(f"""
+AF CASES: {len(AF_CASES)}
+CHF CASES: {len(CHF_CASES)}
+HC CASES: {len(HC_CASES)}
+AR CASES: {len(AR_CASES)}
+MI CASES: {len(MI_CASES)}
+""")
 # %%
 
 # Get R-peaks series using biosppy
