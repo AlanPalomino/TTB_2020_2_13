0b8315b51b91416246f12684d215ac5b59186102
Sebastián <sebastianportillab@gmail.com>
2020-11-24 18:24:03 -0600

    UPDATE Small adjustments for testing

--- TT_utilities.py
+++ TT_utilities.py
@@ -15,13 +15,13 @@ def timeit(func, *args, **kwargs):
     print(f"Function {func.__name__} execution time: {e_time - s_time}")
 
 
-# ================= Importando Bases de Datos 
-class Case2():
+# ================= Importando Bases de Datos
+class Case():
     """
-        Generador del compendio de registros y señales para un caso particular.
-        
-        Object looks for files in the directory provided, to build a list
-        of records from the same case.
+    Generador del compendio de registros y señales para un caso particular.
+
+    Object looks for files in the directory provided, to build a list
+    of records from the same case.
     """
 
     RECORDS = []
@@ -40,6 +40,7 @@ class Case2():
         return f"{5*' * '}End of case {self._case_name}{5*' * '}"
 
     def __getitem__(self, index):
+        "Extract record as a list"
         return self.RECORDS[index]
 
     def _get_records(self):
@@ -54,8 +55,6 @@ class Case2():
             self._get_data(self._case_dir.joinpath(name))
 
     def _get_data(self, path: Path):
-        record = wfdb.rdrecord(str(path))
-        header = wfdb.rdheader(str(path))
         self.RECORDS.append(
             Record(path, self._case_name)
         )
@@ -88,7 +87,7 @@ class Record():
             raise KeyError(f"'{item}' isn't a valid key. Signals in record:{self.sig_names}")
 
     def _get_signals(self):
-        reco = wfdb.rdrecord(str(record_dir))
+        reco = wfdb.rdrecord(str(self.record_dir))
         return reco.p_signal
 
     def plot(self):
