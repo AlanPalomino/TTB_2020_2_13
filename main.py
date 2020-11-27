aba13cf040e085d537b2f84da45ec9e3e255990d
AlanPalomino <zappataville@gmail.com>
2020-11-27 01:28:55 -0600

    Update- Actualización dbs

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ import re
 
 # ===================== Funciones y Métodos ======================= #
 from TT_utilities import *
-
+from TT_utilities import add_moments,add_nonlinear
 
 # ================================================================= #
 # %%
@@ -76,5 +76,31 @@ for record_dir in RECORD_DIRS:
     record_name = re.search("p[0-9]{6}", str(record_dir))[0]
     case = Case(record_dir.joinpath(record_name))
     break
+
+# %%
+#======== Agregando las métricas obtenidas a las Bases 
+print("ACTUALIZANDO DATABASES...")
+#AF_CASES = AF_CASES.apply(add_moments, axis=1)
+#CHF_CASES = CHF_CASES.apply(add_moments, axis=1)
+#HC_CASES = HC_CASES.apply(add_moments, axis=1)
+
+AF_CASES_NL = AF_CASES.copy()
+CHF_CASES_NL = CHF_CASES.copy()
+HC_CASES_NL = HC_CASES.copy()
+print("Métricas agregadas:  ")
+
+print(" - ".join(AF_CASES.columns))
+print(" - ".join(CHF_CASES.columns))
+print(" - ".join(HC_CASES.columns))
+
+print("ACTUALIZANDO DATABASES...")
+AF_CASES_NL = AF_CASES_NL.apply(add_nonlinear, axis=1)
+CHF_CASES_NL = CHF_CASES_NL.apply(add_nonlinear, axis=1)
+HC_CASES_NL = HC_CASES_NL.apply(add_nonlinear, axis=1)
+
+print("Métricas agregadas:  ")
+print(" - ".join(AF_CASES_NL.columns))
+print(" - ".join(CHF_CASES_NL.columns))
+print(" - ".join(HC_CASES_NL.columns))
 
 # %%
