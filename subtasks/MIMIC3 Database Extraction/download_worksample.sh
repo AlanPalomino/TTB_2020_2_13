1c2a98cbb962023d86d24c1a507406792324cedb
Sebastián <sebastianportillab@gmail.com>
2020-12-18 00:16:18 -0600

    ADJUSTMENT  Bash downloaders from DOS to UNIX

--- subtasks/MIMIC3 Database Extraction/download_worksample.sh
+++ subtasks/MIMIC3 Database Extraction/download_worksample.sh
@@ -1,8 +1,7 @@
 
 mkdir atrial_fibrillation_p000638
 cd atrial_fibrillation_p000638
-if [[ ! -d $PWD/p000638 ]]
-then
+if [[ ! -d $PWD/p000638 ]]; then
     wget -r -nc -np -c https://physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000638/
     mv physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000638 .
 fi
@@ -11,8 +10,7 @@ cd ..
                 
 mkdir atrial_fibrillation_p001170
 cd atrial_fibrillation_p001170
-if [[ ! -d $PWD/p001170 ]]
-then
+if [[ ! -d $PWD/p001170 ]]; then
     wget -r -nc -np -c https://physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p001170/
     mv physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p001170 .
 fi
@@ -21,8 +19,7 @@ cd ..
                 
 mkdir atrial_fibrillation_p001354
 cd atrial_fibrillation_p001354
-if [[ ! -d $PWD/p001354 ]]
-then
+if [[ ! -d $PWD/p001354 ]]; then
     wget -r -nc -np -c https://physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p001354/
     mv physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p001354 .
 fi
@@ -31,8 +28,7 @@ cd ..
                 
 mkdir congestive_heartfailure_p000107
 cd congestive_heartfailure_p000107
-if [[ ! -d $PWD/p000107 ]]
-then
+if [[ ! -d $PWD/p000107 ]]; then
     wget -r -nc -np -c https://physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000107/
     mv physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000107 .
 fi
@@ -41,8 +37,7 @@ cd ..
                 
 mkdir congestive_heartfailure_p000906
 cd congestive_heartfailure_p000906
-if [[ ! -d $PWD/p000906 ]]
-then
+if [[ ! -d $PWD/p000906 ]]; then
     wget -r -nc -np -c https://physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000906/
     mv physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000906 .
 fi
@@ -51,8 +46,7 @@ cd ..
                 
 mkdir congestive_heartfailure_p000518
 cd congestive_heartfailure_p000518
-if [[ ! -d $PWD/p000518 ]]
-then
+if [[ ! -d $PWD/p000518 ]]; then
     wget -r -nc -np -c https://physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000518/
     mv physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000518 .
 fi
@@ -61,8 +55,7 @@ cd ..
                 
 mkdir myocardial_infarction_p000154
 cd myocardial_infarction_p000154
-if [[ ! -d $PWD/p000154 ]]
-then
+if [[ ! -d $PWD/p000154 ]]; then
     wget -r -nc -np -c https://physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000154/
     mv physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000154 .
 fi
@@ -71,8 +64,7 @@ cd ..
                 
 mkdir myocardial_infarction_p000154
 cd myocardial_infarction_p000154
-if [[ ! -d $PWD/p000154 ]]
-then
+if [[ ! -d $PWD/p000154 ]]; then
     wget -r -nc -np -c https://physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000154/
     mv physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000154 .
 fi
@@ -81,8 +73,7 @@ cd ..
                 
 mkdir myocardial_infarction_p000123
 cd myocardial_infarction_p000123
-if [[ ! -d $PWD/p000123 ]]
-then
+if [[ ! -d $PWD/p000123 ]]; then
     wget -r -nc -np -c https://physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000123/
     mv physionet.org/static/published-projects/mimic3wdb-matched/1.0/p00/p000123 .
 fi
