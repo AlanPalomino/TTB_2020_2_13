e0362e6e7bc50cdae895a4af8b7bdb05cdc25727
YounkOak <sebastianportillab@gmail.com>
2020-12-25 14:52:31 -0600

    UPDATE check_process display mod

--- check_process.py
+++ check_process.py
@@ -7,5 +7,8 @@ DOWNLOADED = list(Path("Data/").glob("*_p0*"))
 csv_data = pd.read_csv("complete_data.csv")
 PROCESSED = csv_data["case"].unique()
 
-print(f"\nDe los presentes {len(DOWNLOADED)} casos se han procesado {len(PROCESSED)}")
+print(f"\nDe los presentes {len(DOWNLOADED)} casos se han procesado {len(PROCESSED)}\n")
+print(f"Myocardial infarction rows: {len(csv_data[csv_data['cond'] == 'myocardial_infarction'])}")
+print(f"Congestive heart failure rows: {len(csv_data[csv_data['cond'] == 'congestive_heartfailure'])}")
+print(f"Atrial fibrillation rows: {len(csv_data[csv_data['cond'] == 'atrial_fibrillation'])}\n")
 
